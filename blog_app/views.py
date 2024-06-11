b34f4c6df54dfd577377aa4940f0132b6fc2e77f
lewmas9152 <smnjuguna22@gmail.com>
2024-06-11 11:04:38 +0300

    hardcoded some posts

--- blog_app/views.py
+++ blog_app/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+blogs = [{
+    'Title':"My first blog",
+    'Author': "IconSam",
+    'category':"Technology",
+    'content':"I am very happy to be writing my first blog app",
+
+},
+{
+    'Title':"My Second blog",
+    'Author': "Iconicsam",
+    'category':"Tech",
+    'content':"I am very happy to be writing my first django blogApp",
+
+}]
+def home(request):
+    context = {
+        "blogs": blogs
+    }
+    return render(request,'blog_app/home.html',context)

28141c38a32f53f7f57d234d3caf89b45adb2650
lewmas9152 <smnjuguna22@gmail.com>
2024-06-13 20:16:02 +0300

    created login and reg files using auth forms

--- blog_app/urls.py
+++ blog_app/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
     path('',views.home, name='home'),
+    path('register/', views.register_view, name='register'),
+    path('login/',views.login_view, name='login'),
     path('post/<int:id>/',views.post_detail,name='post_detail')
 ]
--- blog_app/views.py
+++ blog_app/views.py
@@ -1,7 +1,33 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404,redirect
+from django.contrib.auth.forms import AuthenticationForm
+from .forms import CustomUserCreationForm
+from django.contrib.auth import login, logout
 from django.http import HttpResponse
 from django .contrib.auth.models import User
 from .models import Post
+
+
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('home')  # Replace 'home' with your desired redirect target
+    else:
+        form = AuthenticationForm()
+    return render(request, 'blog_app/login.html', {'form': form})
+
+def register_view(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')  # Replace 'home' with your desired redirect target
+    else:
+        form = CustomUserCreationForm()
+    return render(request, 'blog_app/register.html', {'form': form})
 
 
 def home(request):

ad40378285a1346af4f712cc601bc6bda8a8b567
lewmas9152 <smnjuguna22@gmail.com>
2024-06-21 09:28:35 +0300

    changed db to postgre

--- blogproj/settings.py
+++ blogproj/settings.py
@@ -65,12 +65,28 @@ WSGI_APPLICATION = 'blogproj.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+'''
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'blogster',  # Replace with your PostgreSQL database name
+        'USER': 'njugush',  # Replace with your PostgreSQL user
+        'PASSWORD': 'password123#@!',  # Replace with your PostgreSQL password
+        'HOST': 'localhost',  # Set to 'localhost' if running locally
+        'PORT': '5432',  # Default PostgreSQL port
+    }
+}
+
+
 
 
 # Password validation

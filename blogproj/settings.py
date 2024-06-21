6bbd948c02e2be68de2679e657969be70728107e
Njuguna Samwel Mugo <115554939+lewmas9152@users.noreply.github.com>
2024-06-21 11:02:51 +0300

    Update settings.py

--- blogproj/settings.py
+++ blogproj/settings.py
@@ -147,6 +147,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+LOGIN_URL = '/login/'
+
 
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
